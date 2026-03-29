92b9bc82020d20993c0025743fb78ab95ce720e1
venkat chaitanya <128158876+venkatmama@users.noreply.github.com>
2026-03-29 18:10:27 +0530

    Update payments.py

--- payments.py
+++ payments.py
@@ -1,9 +1,6 @@
 from telegram import LabeledPrice
 
-PREMIUM_TITLE = "IPLClaw Lifetime 💀"
-PREMIUM_DESC = "Ball-by-ball roasting + paisa vasool insights"
-
-PRICE_STARS = 300
+PRICE = 300
 
 def get_prices():
-    return [LabeledPrice("Lifetime Access", PRICE_STARS)]
+    return [LabeledPrice("Lifetime Access", PRICE)]
